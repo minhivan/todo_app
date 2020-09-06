850500c886e29cd72563395a88db8d8afb8a3b61
minhivan <minhmuofficial@gmail.com>
2020-09-07 01:54:44 +0700

    First day Todo React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Todo from './Todo'
+import {Button,FormControl,Input,InputLabel} from '@material-ui/core';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [todos, setTodos] = useState(['State 1', 'State 2']);
+    const [input, setInput] = useState('');
+    console.log(input);
 
+    const addTodo = (event) =>  {
+        // action when hit button
+        event.preventDefault(); // Stop refresh page
+        console.log("Working !!");
+        setTodos([...todos, input])
+        setInput('')
+    }
+    return (
+        <div className="App">
+            <h1>Hello bro !!</h1>
+            <form>
+                <FormControl>
+                    <InputLabel htmlFor="my-input">Write a todo list here</InputLabel>
+                    <Input id="my-input" aria-describedby="my-helper-text" value={input} onChange={event => setInput(event.target.value)} />
+                </FormControl>
+                <Button disabled={!input} type="submit" variant="contained" color="primary" onClick={addTodo}>
+                    Add todo
+                </Button>
+            </form>
+            <ul>
+                {todos.map(todo => (
+                    <Todo text={todo}/>
+                ))}
+            </ul>
+        </div>
+    );
+}
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCzEAa3B0g_oY1cqEr7J_Nx_k5Z4EOmTcs",
+    authDomain: "instagram-clone-app-af276.firebaseapp.com",
+    databaseURL: "https://instagram-clone-app-af276.firebaseio.com",
+    projectId: "instagram-clone-app-af276",
+    storageBucket: "instagram-clone-app-af276.appspot.com",
+    messagingSenderId: "1004742406134",
+    appId: "1:1004742406134:web:ea9a24753eebc8c790a1d7",
+    measurementId: "G-46QBT4ZPPQ"
+};
+
+const db = firebaseApp.firestore();
+export {db}
