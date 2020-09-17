6f8ba118cd95caa1591668fd74b44311c2fb2826
minhivan <minhmuofficial@gmail.com>
2020-09-18 01:53:52 +0700

    Final + add to firebase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Todo from './Todo'
 import {Button,FormControl,Input,InputLabel} from '@material-ui/core';
 import './App.css';
+import db from './firebase';
+import * as firebase from "firebase";
 
 function App() {
-    const [todos, setTodos] = useState(['State 1', 'State 2']);
+    const [todos, setTodos] = useState([]);
     const [input, setInput] = useState('');
-    console.log(input);
-
+    useEffect(() => {
+        db.collection('todos').orderBy('timestamp','desc').onSnapshot(snapshot => {
+            // console.log(snapshot.docs.map(doc => doc.data().todo));
+            setTodos(snapshot.docs.map(doc => ({id: doc.id, todo: doc.data().todo, date: doc.data().timestamp.toDate().toString() })));
+        });
+    }, [])
     const addTodo = (event) =>  {
         // action when hit button
         event.preventDefault(); // Stop refresh page
-        console.log("Working !!");
-        setTodos([...todos, input])
+        db.collection('todos').add({
+            todo: input,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        setTimeout(() => {
+            console.log("Loadddd")
+        }, 3000);
         setInput('')
     }
     return (
@@ -28,8 +39,8 @@ function App() {
                 </Button>
             </form>
             <ul>
-                {todos.map(todo => (
-                    <Todo text={todo}/>
+                {todos.map((todo) => (
+                    <Todo todo={todo} key={todo.id}/>
                 ))}
             </ul>
         </div>
--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,18 @@
 import React from "react";
-import {List, ListItem, ListItemText} from '@material-ui/core';
-
+import {List, ListItem, ListItemText, Button} from '@material-ui/core';
+import db from './firebase'
 function Todo(props) {
+    const delTodo = (event) => {
+        event.preventDefault();
+        db.collection('todos').doc(props.todo.id).delete();
+    }
     return (
         <List>
             <ListItem>
-                <ListItemText primary={props.text} secondary="Dummy deadline !!!"/>
+                <ListItemText primary={props.todo.todo} secondary={props.todo.date} />
+                <Button variant="contained" color="secondary" onClick={ delTodo }>
+                    Delete
+                </Button>
             </ListItem>
         </List>
     )
